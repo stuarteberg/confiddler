57fa64a2efecefe53f08bac051c42239e5d5c1b9
Stuart Berg <bergs@janelia.hhmi.org>
2020-01-06 15:12:46 -0500

    Re-run versioneer to fix __version__

--- confiddler/__init__.py
+++ confiddler/__init__.py
@@ -1,3 +1,7 @@
 from jsonschema import ValidationError
 from .core import load_config, dump_config, dump_default_config, validate, emit_defaults, flow_style, convert_to_base_types
 from . import json
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
