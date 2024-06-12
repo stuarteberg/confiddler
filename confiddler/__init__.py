effd3c0ad7b1a8df8896703ae168c78e82d82f13
Stuart Berg <bergs@janelia.hhmi.org>
2024-06-11 22:35:48 -0400

    Update versioneer

--- confiddler/__init__.py
+++ confiddler/__init__.py
@@ -2,6 +2,5 @@ from jsonschema import ValidationError
 from .core import load_config, dump_config, dump_default_config, validate, emit_defaults, flow_style, convert_to_base_types
 from . import json
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
